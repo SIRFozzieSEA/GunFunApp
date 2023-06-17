2f71561a622e084032daa0bfc1bec9af01663452
SIRFozzieSEA <github@codef.com>
2023-06-16 20:56:11 -0700

    prop change

--- src/main/java/com/codef/gunfunapp/controllers/MainController.java
+++ src/main/java/com/codef/gunfunapp/controllers/MainController.java
@@ -39,7 +39,7 @@ public class MainController {
 	@Qualifier("jdbcMaster")
 	private JdbcTemplate jdbcTemplateOne;
 	
-	@Value("${spring.datasource.url}")
+	@Value("${spring.datasource.jdbcUrl}")
 	private String jdbcPath;
 	
 	@Value("${spring.datasource.driverClassName}")
